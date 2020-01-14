6a477b2b4b357c4aa72a892c4aef829b06a17ded
Asthenosphere <e0271247@u.nus.edu>
2020-01-15 00:06:20 +0800

    Use external semantic stylesheet

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,7 +4,6 @@
 
 import React from "react";
 import { render } from "react-dom";
-import 'semantic-ui-css/semantic.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
