3863fdc8dd5cb0cdf1edf8588a8ef26a349b2a27
Asthenosphere <e0271247@u.nus.edu>
2020-01-15 09:57:56 +0800

    Change url to production

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -5,6 +5,7 @@
 import React from "react";
 import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'sem
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
